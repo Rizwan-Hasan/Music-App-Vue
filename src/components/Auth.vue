3d8cde1351ee132311b037e2474c50db6b895d3d
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2024-10-07 11:54:08 +0600

    updated

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -86,6 +86,13 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div
+            class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            v-bind:class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             v-bind:validation-schema="schema"
@@ -186,6 +193,7 @@
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              v-bind:disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -217,6 +225,10 @@ export default {
       userData: {
         country: 'Bangladesh',
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created.',
     };
   },
   computed: {
@@ -228,6 +240,13 @@ export default {
   methods: {
     register(form_data) {
       console.log(form_data);
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = 'bg-blue-500';
+      this.reg_alert_msg = 'Please wait! Your account is being created.';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg = 'Success! Your account has been created.';
     },
   },
 };
